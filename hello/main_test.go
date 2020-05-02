77aa58001b057e06a832f447034f99f8d3991030
Didier Roland Sitou <dsitoufr@yahoo.fr>
2020-05-02 17:22:14 +0000

    Update main_test.go

--- hello/main_test.go
+++ hello/main_test.go
@@ -1,1 +1,13 @@
 
+package main
+
+import (
+	"testing"
+)
+
+func TestGCE(t *testing.T) {
+	i := newInstance()
+	//if !metadata.OnGCE() && i.Error != "Not running on GCE" {
+		t.log("Unit tests completed, but error does not indicate that fact.")
+	}
+}
